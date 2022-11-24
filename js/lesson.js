a71b8999e1bf594d73c477928da34fd5392c5593
Naadir Mohamed <smartnaadir@gmail.com>
2022-11-24 12:44:16 +0400

    5th Commit

---
+++ js/lesson.js
@@ -0,0 +1,83 @@
+//lesson.js
+let modules = [
+    {
+        "id": 10,
+        "subject": "Mathematics",
+        "location": "London",
+        "price": 100,
+        "spaces": 5,
+        "image": "images/math.jpg"
+    },
+    {
+        "id": 20,
+        "subject": "English",
+        "location": "Oxford",
+        "price": 200,
+        "spaces": 5,
+        "image": "images/english.jpg"
+    },
+    {
+        "id": 30,
+        "subject": "Biology",
+        "location": "Hendon",
+        "price": 400,
+        "spaces": 5,
+        "image": "images/biology.png"
+    },
+    {
+        "id": 40,
+        "subject": "Computer Science",
+        "location": "Cambridge",
+        "price": 250,
+        "spaces": 5,
+        "image": "images/computer.png"
+    },
+    {
+        "id": 50,
+        "subject": "Social Studies",
+        "location": "Bristol",
+        "price": 300,
+        "spaces": 5,
+        "image": "images/social.png"
+    },
+    {
+        "id": 60,
+        "subject": "Physics",
+        "location": "Brimingham",
+        "price": 600,
+        "spaces": 5,
+        "image": "images/physics.png"
+    },
+    {
+        "id": 70,
+        "subject": "Arts",
+        "location": "Bristol",
+        "price": 150,
+        "spaces": 5,
+        "image": "images/arts.png"
+    },
+    {
+        "id": 80,
+        "subject": "Economics",
+        "location": "Watford",
+        "price": 245,
+        "spaces": 5,
+        "image": "images/economics.png"
+    },
+    {
+        "id": 90,
+        "subject": "Business Studies",
+        "location": "Southampton",
+        "price": 445,
+        "spaces": 5,
+        "image": "images/business.png"
+    },
+    {
+        "id": 100,
+        "subject": "Chemistry",
+        "location": "Cambridge",
+        "price": 650,
+        "spaces": 5,
+        "image": "images/chemistry.png"
+    }
+]
